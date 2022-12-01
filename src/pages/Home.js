04bab0719efe5122e6bd47fff53310d51f63851d
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-01 20:19:55 +0600

    data show successfully

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,13 @@ import { useProducts } from "../hooks/useProducts";
 
 const Home = () => {
 
-  const { state: { products } } = useProducts()
-  console.log(products.data)
+  const { state } = useProducts()
+  const {data : products} = state.products
+  console.log(products)
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       {
-        products.data.map(product => <ProductCard product={product} />)
+        products?.map(product => <ProductCard product={product} />)
       }
     </div>
   );
