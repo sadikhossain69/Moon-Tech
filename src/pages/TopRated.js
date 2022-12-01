3b838637c2de3a6defc08d8c06e650ddc4c57da5
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-01 20:47:13 +0600

    top rated filter

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -1,9 +1,31 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProducts } from "../hooks/useProducts";
 
 const TopRated = () => {
+  const { state } = useProducts()
+  const { loading, products, error } = state
+  const { data: p } = products
+
+  let content
+
+  if (loading) {
+    content = <p>Loading...</p>
+  }
+
+  if (error) {
+    content = <p>Something went wrong</p>
+  }
+
+  if (!loading && !error) {
+    content = p?.filter(product => product.rating >= 4 ).map(product => <ProductCard key={product._id} product={product} />)
+  }
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is featured page</h1>
+      {
+        content
+      }
     </div>
   );
 };
