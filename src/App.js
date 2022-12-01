15d85f5ca567bb0fb0f748d782148a8230e77557
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-01 18:32:01 +0600

    axios added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import axios from "axios";
+import { createContext, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
 
+const PRODUCT_CONTEXT = createContext()
+
 function App() {
+
+  const getProducts = async () => {
+    const { data } = await axios.get('https://moon-tech-server-odfw.onrender.com/products')
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <div>
       <RouterProvider router={routes} />
